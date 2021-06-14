690d72a265ba9a222aec00baa6a733805bfd1c08
Felipe <felipeffggomes@hotmail.com>
2021-06-14 13:28:08 -0300

    Adicionando conteudo de tratamento de exceções

--- src/Fluxo.java
+++ src/Fluxo.java
@@ -2,7 +2,14 @@ public class Fluxo {
 
     public static void main(String[] args) {
         System.out.println("Ini do main");
-        metodo1();
+        try {
+            metodo1();
+        }
+       catch (ArithmeticException |NullPointerException ex) {   //ex é uma referencia da classe da exceção
+            String msg = ex.getLocalizedMessage();              //pega a mensagem de erro.
+            System.out.println("Exception " + msg);
+            ex.printStackTrace();                               //printa o rastro do erro.
+        }
         System.out.println("Fim do main");
     }
 
@@ -14,8 +21,11 @@ public class Fluxo {
 
     private static void metodo2() {
         System.out.println("Ini do metodo2");
-        for(int i = 1; i <= 5; i++) {
+        for (int i = 1; i <= 5; i++) {
             System.out.println(i);
+            int a = i / 0;
+            //Conta c = null;
+            //c.deposita();
         }
         System.out.println("Fim do metodo2");
     }
